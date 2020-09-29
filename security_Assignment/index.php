d22a3354005e095b1de3a3f3781697e1227da47f
LhetMagnan <lmagnan17@alustudent.com>
2020-09-29 19:22:14 +0200

    Final Work for security Assignment

---
+++ security_Assignment/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<ul class="blue">
+    <li><a href="#" title="home">Home</a></li>
+    <li><a href="#" title="about">About</a></li>
+    <li><a href="login.php" title="signin">Login</a></li>
+    <li><a href="register.php" title="signup">Sign up</a></li>
+</ul>
+</body>
+</html><?php
